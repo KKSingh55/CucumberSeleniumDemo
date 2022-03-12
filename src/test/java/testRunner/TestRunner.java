7a0354346b93fb1b03f6dc426f87cfea798dfeda
kanhaiya <kksingh11013@gmail.com>
2022-03-12 18:50:08 +0530

    new commit

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features=".//Features//",
+		features=".//Features//customer.feature",
 		glue="stepDefination",
 		dryRun = false,
 		monochrome=true,
